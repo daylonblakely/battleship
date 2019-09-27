145805509138500e0a2b85af7845d3d80c4b4444
Daylon <daylonblakely@gmail.com>
2019-09-26 22:21:48 -0700

    Added a make_guess function that changes the global guess variables when a player presses a button

--- battleship_gui.py
+++ battleship_gui.py
@@ -28,7 +28,7 @@ class Player:
         for x in range(5):
             for y in range(5):
                 btn = tk.Button(self.opponent_board, text="O", command=
-                lambda frame=self.opponent_board, row=x, col=y: do_something(frame, row, col))
+                lambda window=self.win, row=x, col=y: make_guess(window, row, col))
                 btn.grid(row=x+1, column=y, sticky=tk.W, padx=2, pady=2)
 
         #Create players board
@@ -41,9 +41,53 @@ class Player:
             lbl.grid(row=i, column=0, sticky=tk.W, padx=2, pady=5)
             i += 1
 
+game_over = False
+guess_row = 0
+guess_col = 0
 
-def do_something(frame, x=0, y=0):
+def make_guess(window, x=0, y=0):
+    global guess_row
+    global guess_col
     print("{}, {}".format(x + 1, y + 1))
+    # set the global guess variables to the x and y
+    guess_row = x+1
+    guess_col = y+1
+    # hide the player's window after they make a guess
+    window.withdraw()
+
+def take_turn(this_player, opponent):
+    global game_over
+    this_player.print_board()
+    #loop to check for valid input
+    done = False
+    while not done:
+        try:
+            guess_row = int(input("Guess row:")) -1
+            guess_col = int(input("Guess col:")) -1
+            if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+                raise
+            done = True
+        except:
+            print("INVALID INPUT! Please enter values between 1 and 5")
+
+    #already guessed
+    if (this_player.opponent_board[guess_row][guess_col] == "M") or (
+            this_player.opponent_board[guess_row][guess_col] == "H"):
+        print("You guessed that one already!")
+    #correct guess
+    elif opponent.board[guess_row][guess_col] == "X":
+        print("HIT!")
+        this_player.opponent_board[guess_row][guess_col] = "H"
+        opponent.board[guess_row][guess_col] = "H"
+        #check for winner, compare hits on this_player.opponent_board with opponent.board
+        if sum(x.count("X") for x in opponent.board) == 0:
+            print(this_player.get_name() + " wins!")
+            game_over = True
+    #incorrect guess
+    elif opponent.board[guess_row][guess_col] == "O":
+        print("MISS!")
+        this_player.opponent_board[guess_row][guess_col] = "M"
+        opponent.board[guess_row][guess_col] = "M"
 
 def main():
     root = tk.Tk()
