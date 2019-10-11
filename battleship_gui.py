fd24a99a5e19f24bbd2c9e788cf9ca1bc8707dfd
Daylon <daylonblakely@gmail.com>
2019-10-11 11:34:19 -0700

    Moved mainloop() code outside of main function. Made players scores show with a StringVar that gets set every time a game ends.

--- battleship_gui.py
+++ battleship_gui.py
@@ -7,10 +7,10 @@ class Player:
         self.score = 0
         self.name = name
         self.root = root
-        #create player's board
+        # create player's board
         self.players_board = []
         self.create_ships()
-        #create toplevel window for player's boards, use withdraw to hide the window when program starts
+        # create toplevel window for player's boards, use withdraw to hide the window when program starts
         self.win = tk.Toplevel(root)
         self.win.withdraw()
         self.win.title("Battleship")
@@ -29,29 +29,30 @@ class Player:
             while not done:
                 ship_row = randint(0, len(self.players_board) - 1)
                 ship_col = randint(0, len(self.players_board) - 1)
-                #ship_col = randint(0, len(board) - 1)
+                # ship_col = randint(0, len(board) - 1)
                 if self.players_board[ship_row][ship_col] == "O":
                     self.players_board[ship_row][ship_col] = "X"
                     done = True
 
-    #opens the players window from being withdrawn
+    # opens the players window from being withdrawn
     def open_window(self):
         self.win.deiconify()
 
-    #display the opponent's and player's board in the toplevel window
+    # display the opponent's and player's board in the toplevel window
     def create_opponent_board(self):
-        #create opponent's board for guessing
+        # create opponent's board for guessing
         self.opponent_board.grid(row=0, column=0, sticky=tk.W, padx=15)
         tk.Label(self.opponent_board, text="Opponent's board").grid(row=0, column=0, columnspan=5, sticky=tk.W, pady=2)
-        #each button has its own x and y to be passed to a function
+        # each button has its own x and y to be passed to a function
         for x in range(5):
             for y in range(5):
                 btn = tk.Button(self.opponent_board, text="O", command=
                 lambda player=self, window=self.win, frame=self.opponent_board, row=x, col=y:
                     make_guess(player, window, frame, row, col))
                 btn.grid(row=x+1, column=y, sticky=tk.W, padx=2, pady=2)
+
     def create_player_board(self):
-        #Create players board
+        # Create players board
         self.board.grid(row=0, column=1, sticky=tk.W, padx=15)
         tk.Label(self.board, text=self.name + " board").grid(row=0, column=0, sticky=tk.W, pady=2)
         # show players board as a grid of labels
@@ -61,9 +62,25 @@ class Player:
             lbl.grid(row=i, column=0, sticky=tk.W, padx=2, pady=5)
             i += 1
 
+
 game_over = False
-player_going = None
-player_not_going = None
+root = tk.Tk()
+root.title("Battleship")
+
+player1 = Player("Player 1", root)
+player2 = Player("Player 2", root)
+# let player1 go first
+player_going = player1
+player_not_going = player2
+
+play = tk.Button(root, text="Play", command=player1.open_window).pack()
+var1 = tk.StringVar()
+var1.set("Player 1 Score: " + str(player1.score))
+var2 = tk.StringVar()
+var2.set("Player 2 Score: " + str(player2.score))
+p1 = tk.Label(root, textvariable=var1).pack()
+p2 = tk.Label(root, textvariable=var2).pack()
+
 
 def make_guess(player, window, f, x=0, y=0):
     print("{}, {}".format(x + 1, y + 1))
@@ -84,17 +101,20 @@ def make_guess(player, window, f, x=0, y=0):
         player_not_going.open_window()
     if game_over:
         game_over = False
+        var1.set("Player 1 Score: " + str(player1.score))
+        var2.set("Player 2 Score: " + str(player2.score))
+
 
 def take_turn(frame, guess_row, guess_col):
     global game_over
     global player_going
     global player_not_going
 
-    #already guessed
+    # already guessed
     if (player_not_going.players_board[guess_row][guess_col] == "M") or (
             player_not_going.players_board[guess_row][guess_col] == "H"):
         print("You guessed that one already!")
-    #correct guess
+    # correct guess
     elif player_not_going.players_board[guess_row][guess_col] == "X":
         print("HIT!")
         # changes the opponents board and update
@@ -104,12 +124,12 @@ def take_turn(frame, guess_row, guess_col):
         button = frame.grid_slaves(guess_row + 1)[4-guess_col]
         button["text"] = "H"
         button["bg"] = "green"
-        #check for winner, compare hits on this_player.opponent_board with opponent.board
+        # check for winner, compare hits on this_player.opponent_board with opponent.board
         if sum(x.count("X") for x in player_not_going.players_board) == 0:
             print(player_going.name + " wins!")
             player_going.score += 1
             game_over = True
-            #reset each player's boards so they can play again
+            # reset each player's boards so they can play again
             player_going.win.withdraw()
             player_going.create_opponent_board()
             player_going.create_ships()
@@ -117,11 +137,10 @@ def take_turn(frame, guess_row, guess_col):
             player_not_going.create_opponent_board()
             player_not_going.create_ships()
             player_not_going.create_player_board()
-            player_going.root.update()
-    #incorrect guess
+    # incorrect guess
     elif player_not_going.players_board[guess_row][guess_col] == "O":
         print("MISS!")
-        #change opponents board and update
+        # change opponents board and update
         player_not_going.players_board[guess_row][guess_col] = "M"
         player_not_going.create_player_board()
         # change the players board
@@ -129,24 +148,5 @@ def take_turn(frame, guess_row, guess_col):
         button["text"] = "M"
         button["bg"] = "red"
 
-def main():
-    root = tk.Tk()
-    root.title("Battleship")
 
-    global player_going
-    global player_not_going
-    player1 = Player("Player 1", root)
-    player2 = Player("Player 2", root)
-    # let player1 go first
-    player_going = player1
-    player_not_going = player2
-
-    open_p1 = tk.Button(root, text="Open Player 1's Window", command=player1.open_window).pack()
-    open_p2 = tk.Button(root, text="Open Player 2's Window", command=player2.open_window).pack()
-    play = tk.Button(root, text="Play", command=player1.open_window).pack()
-    p1 = tk.Label(root, text="Player 1 Score: "+str(player1.score)).pack()
-    p2 = tk.Label(root, text="Player 2 Score: " + str(player2.score)).pack()
-
-    root.mainloop()
-
-main()
+root.mainloop()
